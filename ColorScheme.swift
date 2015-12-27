52ce178605582226a590314cd3bb1c58b7245a5c
Stefan Arambasich <ArtisOracle@gmail.com>
2015-12-27 00:31:28 -0500

    Sym to hardlink

--- ColorScheme.swift
+++ ColorScheme.swift
@@ -1,1 +1,47 @@
-SAColorScheme/ColorScheme.swift
+//
+//  ColorScheme.swift
+//  SAColorScheme
+//
+//  Created by Stefan Arambasich on 12/27/2015.
+//
+//  Copyright (c) 2015-2016 Stefan Arambasich. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+import UIKit
+
+struct ColorScheme {
+    /// The primary color
+    var primaryColor: UIColor
+    /// The secondary color
+    var secondaryColor: UIColor
+    /// An accent color
+    var accentColor: UIColor
+    /// Content fill color
+    var fillColor: UIColor
+    /// The color of primary text
+    var primaryTextColor: UIColor
+    /// The color of secondary text
+    var secondaryTextColor: UIColor
+    /// The color of text that isn't primary or secondary
+    var textColor: UIColor
+    /// A divider color
+    var dividerColor: UIColor
+}
